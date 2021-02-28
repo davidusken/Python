a680e071a2106617aaeca71fe760d3544fce5904
David Usken <davidusken@gmail.com>
2021-02-28 23:09:47 +0100

    1.02

--- calculator v1.py
+++ calculator v1.py
@@ -1,56 +1,59 @@
 import time
 
-print("What do you wish to do?")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-print("Enter: ") 
-operationChoice = input()
+operationChoice = None 
 
-if (operationChoice == "1") or (operationChoice == "1.") or (operationChoice == "Add") or (operationChoice == "add"):
-    print("Enter the first number you want to add")
+def choice_function():
+    print("What do you wish to do?")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("Enter: ", end="") 
+    return input()
+
+operationChoice = choice_function() 
+operationChoice = operationChoice.lower()
+operationChoice = operationChoice.replace(".", "")
+
+if (operationChoice == "1") or (operationChoice == "add"):
+    print("Enter the first number you want to add: ", end="")
     addNumberA = input()
-    print ("Ok, and the second one?")
+    print ("Ok, and the second one: ", end="")
     addNumberB = input()
     sum = int (addNumberA) + int (addNumberB)
     print ("Performing extreme calculations...")
     time.sleep(1)
     print ("The sum of {0} + {1} is {2}" .format(addNumberA, addNumberB, sum))
 
-elif (operationChoice == "2") or (operationChoice == "2.") or (operationChoice == "Subtract") or (operationChoice == "subtract"):
-    print("Enter the first number you want to subtract")
-    addNumberA = input()
-    print ("Ok, and the second one?")
-    addNumberB = input()
-    sum = int (addNumberA) - int (addNumberB)
+elif (operationChoice == "2") or (operationChoice == "subtract"):
+    print("Enter the first number you want to subtract: ", end="")
+    subNumberA = input()
+    print ("Ok, and the second one: ", end="")
+    subNumberB = input()
+    sum = int (subNumberA) - int (subNumberB)
     print ("Performing extreme calculations...")
     time.sleep(1)
-    print ("The sum of {0} - {1} is {2}" .format(addNumberA, addNumberB, sum))
+    print ("The sum of {0} - {1} is {2}" .format(subNumberA, subNumberB, sum))
 
-elif (operationChoice == "3") or (operationChoice == "3.") or (operationChoice == "Multiply") or (operationChoice == ("multiply")):
-    print("Enter the first number you want to multiply")
-    addNumberA = input()
-    print ("Ok, and the second one?")
-    addNumberB = input()
-    sum = int (addNumberA) - int (addNumberB)
+elif (operationChoice == "3") or (operationChoice == ("multiply")):
+    print("Enter the first number you want to multiply: ", end="")
+    mulNumberA = input()
+    print ("Ok, and the second one: " , end="")
+    mulNumberB = input()
+    sum = int (mulNumberA) - int (mulNumberB)
     print ("Performing extreme calculations...")
     time.sleep(1)
-    print ("The sum of {0} * {1} is {2}" .format(addNumberA, addNumberB, sum))
+    print ("The sum of {0} * {1} is {2}" .format(mulNumberA, mulNumberB, sum))
 
-elif (operationChoice == "4") or (operationChoice == "4.") or (operationChoice == "Divide") or (operationChoice == "divide"):
-    print("Enter the first number you want to divide")
-    addNumberA = input()
-    print ("Ok, and the second one?")
-    addNumberB = input()
-    sum = int (addNumberA) - int (addNumberB)
+elif (operationChoice == "4") or (operationChoice == "divide"):
+    print("Enter the first number you want to divide: ", end="")
+    divNumberA = input()
+    print ("Ok, and the second one: ", end="")
+    divNumberB = input()
+    sum = int (divNumberA) - int (divNumberB)
     print ("Performing extreme calculations...")
     time.sleep(1)
-    print ("The sum of {0} divided by {1} is {2}" .format(addNumberA, addNumberB, sum))      
+    print ("The sum of {0} divided by {1} is {2}" .format(divNumberA, divNumberB, sum))      
 else:
     print("User input not recognized, please try again.")
-
-### Future improvements:
-# Add squaring as a operation
-# Easter egg for special numbers
-# Allow text input from user e.g (52 * 12) + 90 / 3
+    operationChoice = choice_function()
