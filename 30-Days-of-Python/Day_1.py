4ae80ac66cec689bd004d7d79de46c555ed37197
David Usken <davidusken@gmail.com>
2021-03-08 16:52:24 +0100

    1.0

---
+++ 30-Days-of-Python/Day_1.py
@@ -0,0 +1,18 @@
+# Day 1: Numbers, Arithmetic, and Printing to the Console
+
+# Exercises
+# Now that we’ve been through the material for this post, here are a few exercises so that you can practice:
+
+# Print your age to the console.
+
+print("My age is 28.")
+
+# Calculate and print the number of days, weeks, and months in 27 years. Don’t worry about leap years!
+
+print(365 * 27)
+print(52 * 27)
+print(12 * 27)
+
+# Calculate and print the area of a circle with a radius of 5 units. You can be as accurate as you like with the value of pi.
+
+print(3.14 * 5 * 5)
