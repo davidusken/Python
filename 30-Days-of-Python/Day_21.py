56d2103d6be1e7be850ba87a1ba1e113333ddf13
David Usken <davidusken@gmail.com>
2021-04-01 00:27:40 +0200

    1.0

---
+++ 30-Days-of-Python/Day_21.py
@@ -0,0 +1,7 @@
+# Day 21: Splitting Code Into Multiple Files
+
+# Exercises
+# For today's (only) exercise, we're giving you a bunch of code that is all in one file.
+# Your task is to split that code into multiple files. You can choose how many and which files you want to split the code into, but think about why you're putting each piece on code in each file!
+
+print("Did this locally.")
