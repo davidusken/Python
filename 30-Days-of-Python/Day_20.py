7ca49d22fb51cee4c04bedd9252db55d8981ae45
David Usken <davidusken@gmail.com>
2021-04-01 00:09:20 +0200

    1.0

--- 30-Days-of-Python/Day_20.py
+++ 30-Days-of-Python/Day_20.py
@@ -25,28 +25,12 @@ names = ("bob", "Christopher", "Rachel", "MICHAEL", "jessika", "francine")
 
 # Use a list comprehension with a filtering condition so that only names with fewer than 8 characters end up in the new list. Make sure that every name in the new list is in title case.
 
-# Doing it in a normal loop first
-
-namelist = []
-
-for name in names:
-	len(name)
-	if len(name) < 8:
-			name.title()
-			namelist.append(name)
-			print(namelist)
-
-
-# def namelength(names):
-#     return len(names)
-
-# for name in names:
-#     if namelength > 8 == 0
-
-#shorternames = filter
-
-# print(namelength(names))
-# #names = filter[]
-# print(names)
+names = [name.title() for name in names if len(name) < 8]
+print(names)
 
 # Use filter to remove all negative numbers from the following range: range(-5, 11). Print the remaining numbers to the console.
+
+def positivenum(number):
+	return number >= 0
+
+print(*filter(positivenum, range(-5, 11)))
